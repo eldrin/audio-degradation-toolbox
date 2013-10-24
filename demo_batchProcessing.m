4e238f476813a39bf4fcb4e4d49c9cc9b54bc395
matthiasm <1130046+matthiasmauch@users.noreply.github.com>
2013-10-24 20:47:50 +0100

    moved writeCsvFile out to extra file

--- demo_batchProcessing.m
+++ demo_batchProcessing.m
@@ -51,7 +51,9 @@ degradationnames = {'liveRecording', ...
                     'unit_addSound', ...
                     'unit_applyAliasing', ...
                     'unit_applyClippingAlternative', ...
-                    'unit_applyHarmonicDistortion'};
+                    'unit_applyHarmonicDistortion', ...
+                    'unit_applyMp3Compression', ...
+                    'unit_applySpeedup'};
 nDegradation = length(degradationnames);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -136,26 +138,6 @@ parfor k=1:numLines  % use this if you have the parallel computing toolbox
         end
     end
 end
-
-end
-
-
-function writeCsvFile(filename, cols_numeric, remainingColsCell)
-
-numberOfAdditionalColumns = size(remainingColsCell,2);
-
-fid = fopen(filename,'w');
-for m=1:size(cols_numeric,1)
-    fprintf(fid,'%f',cols_numeric(m,1));
-    for n=2:size(cols_numeric,2)
-        fprintf(fid,' %f',cols_numeric(m,n));
-    end
-    for n=1:numberOfAdditionalColumns
-        fprintf(fid,' %s',remainingColsCell{m,n});
-    end
-    fprintf(fid,'\n');
-end
-fclose(fid);
 
 end
 
